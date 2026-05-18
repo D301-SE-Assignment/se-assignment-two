de9592b24eb33107c22437203b6d7c6cfe74078c
taswin <taswin@taswin.net>
2026-05-18 16:17:01 +1200

    Made skeleton Login and Register pages

--- NutriTrack/app/(pages)/login.tsx
+++ NutriTrack/app/(pages)/login.tsx
@@ -1,13 +1,27 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet, View, ScrollView, Text, TextInput } from 'react-native';
+import { Platform, StyleSheet, View, ScrollView, Button, Text, TextInput, Pressable } from 'react-native';
 
-import { Link } from 'expo-router';
-import { useRoute } from '@react-navigation/native';
+function login()
+{
+  alert("Login")
+}
+function register()
+{
+  alert("Register")
+}
 
 export default function HomeScreen() {
   return (
-    <ScrollView>
-      <Text>{useRoute().name}</Text>
-    </ScrollView>
-  );
+    <View className="justify-center p-10 flex-1">
+      <Text className="text-4xl font-bold self-center mb-4">Nutri-Track</Text>
+      
+      <Text className="text-xl">Email Address</Text>
+      <TextInput className="border rounded-full p-2 mb-2" autoComplete='email' autoFocus keyboardType='email-address' returnKeyType='next' placeholder='example@example.com'></TextInput>
+      
+      <Text className="text-xl">Password</Text>
+      <TextInput  className="border rounded-full p-2 mb-4" autoComplete='email' keyboardType='default' returnKeyType='go' placeholder='example@example.com'></TextInput>
+      
+      <Pressable className="bg-blue-500 text-white self-center border rounded-full py-1 px-2 w-min" onPress={login}><Text>Login</Text></Pressable>
+      <Pressable className="self-center" onPress={register}><Text>Don't have an Account? <Text className="text-blue-500">Register</Text></Text></Pressable>
+    </View>
+  )
 };
