57ec4801d63e3fdcde2897a9e14051e2acb830b5
Tasman (23366844) <23366844@studentmail.ucol.ac.nz>
2026-05-27 15:44:01 +1200

    added optional drop table commands to `@\assets\db\nutritrack.tsx>migration`

--- NutriTrack/assets/db/nutri-track.tsx
+++ NutriTrack/assets/db/nutri-track.tsx
@@ -1,19 +1,20 @@
 export const migration =
 `
+    /*
+    PRAGMA foreign_keys = OFF;
+
+    DROP TABLE IF EXISTS "sessions";
+    DROP TABLE IF EXISTS "profiles";
+    DROP TABLE IF EXISTS "users";
+
+    PRAGMA foreign_keys = ON;
+    */
+
     PRAGMA journal_mode = WAL;
     CREATE TABLE IF NOT EXISTS "users" (
         "id" INTEGER PRIMARY KEY,
         "email" TEXT NOT NULL UNIQUE,
         "password" TEXT NOT NULL
-    );
-
-    CREATE TABLE IF NOT EXISTS "sessions" (
-        "token" TEXT PRIMARY KEY,
-        "user_id" INTEGER NOT NULL,
-        "profile_id" INTEGER,
-        "expiry" INTEGER,
-        FOREIGN KEY ("user_id") REFERENCES "users" ("id"),
-        FOREIGN KEY ("profile_id") REFERENCES "profiles" ("id")
     );
 
     CREATE TABLE IF NOT EXISTS "profiles" (
@@ -26,5 +27,14 @@ export const migration =
         "dietary_requirements" TEXT,
         "medical_conditions" TEXT,
         FOREIGN KEY ("user_id") REFERENCES "users" ("id")
+    );
+
+    CREATE TABLE IF NOT EXISTS "sessions" (
+        "token" TEXT PRIMARY KEY,
+        "user_id" INTEGER NOT NULL,
+        "profile_id" INTEGER,
+        "expiry" INTEGER,
+        FOREIGN KEY ("user_id") REFERENCES "users" ("id"),
+        FOREIGN KEY ("profile_id") REFERENCES "profiles" ("id")
     );
 `
