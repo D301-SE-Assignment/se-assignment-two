d130e401a280dc60e60194afb5eecc3d2aa49c3a
Tasman (23366844) <23366844@studentmail.ucol.ac.nz>
2026-06-04 11:46:36 +1200

    finished adding tables to schema

--- NutriTrack/assets/db/schema.tsx
+++ NutriTrack/assets/db/schema.tsx
@@ -1,5 +1,4 @@
-import { sqliteTable, integer, text } from "drizzle-orm/sqlite-core"
-import * as sqlitecore from "drizzle-orm/sqlite-core"
+import { sqliteTable, integer, text, real, primaryKey } from "drizzle-orm/sqlite-core"
 
 export const users = sqliteTable('users',
     {
@@ -21,6 +20,7 @@ export const profiles = sqliteTable('profiles',
         medical_conditions: text('medical_conditions'),
     }
 )
+
 export const sessions = sqliteTable('sessions',
     {
         token: text('token').primaryKey(),
@@ -28,6 +28,58 @@ export const sessions = sqliteTable('sessions',
         profile_id: integer('profile_id').references(() => profiles.id),
         expiry: integer('expiry').notNull(),
     }
+)
+
+export const record_types = sqliteTable('record_types',
+    {
+        id: integer('id').primaryKey({ autoIncrement: true }),
+        type_name: text('type_name').notNull(),
+        units: text('units').notNull()
+    }
+)
+
+export const profile_records = sqliteTable('profile_records',
+    {
+        profile_id: integer('profile_id').references(() => profiles.id),
+        record_type: integer('record_type').references(() => record_types.id),
+        measured_at: integer('measured_at').notNull(),
+        value: real('value').notNull()
+    }, (table) =>
+    [
+        primaryKey({ columns: [table.profile_id, table.record_type] })
+    ]
+)
+
+export const meals = sqliteTable('meals',
+    {
+        id: integer('id').primaryKey({ autoIncrement: true }),
+        name: text('name').notNull(),
+        description: text('description'),
+        calories: integer('calories').notNull(),
+        nutrition_information: text('nutrition_information')
+    }
+)
+
+export const meal_records = sqliteTable('meal_records',
+    {
+        profile_id: integer('profile_id').references(() => profiles.id),
+        meal_id: integer('meal_id').references(() => meals.id),
+        eaten_at: text('eaten_at').notNull(),
+        quantity: real('quantity').notNull(),
+    }, (table) =>
+    [
+        primaryKey({ columns: [table.profile_id, table.meal_id] })
+    ]
+)
+
+export const favourite_meals = sqliteTable('favourite_meals',
+    {
+        user_id: integer('user_id').references(() => users.id),
+        meal_id: integer('meal_id').references(() => meals.id),
+    }, (table) =>
+    [
+        primaryKey({ columns: [table.user_id, table.meal_id] })
+    ]
 )
 
 export type User = typeof users.$inferSelect
