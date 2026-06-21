fb0e34d2a349a585f41f8e715790fc20b35a1aaf
vyaspot <23366840@studentmail.ucol.ac.nz>
2026-06-21 15:52:41 +1200

    Deleted the test file which i feel unnecessary now.

--- NutriTrack/metro.config.js
+++ NutriTrack/metro.config.js
@@ -1,7 +1,19 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
- 
+const { withNativeWind } = require("nativewind/metro");
+
 const config = getDefaultConfig(__dirname);
-config.resolver.assetExts.push('wasm');
- 
-module.exports = withNativeWind(config, { input: './global.css' });
+config.resolver.assetExts.push("wasm");
+
+// Required for expo-sqlite's web backend (OPFS-based SQLite WASM worker)
+config.server = {
+  ...config.server,
+  enhanceMiddleware: (middleware) => {
+    return (req, res, next) => {
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+      return middleware(req, res, next);
+    };
+  },
+};
+
+module.exports = withNativeWind(config, { input: "./global.css" });
