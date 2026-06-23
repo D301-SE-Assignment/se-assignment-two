fa997e949eb8ced6e8207b2ad42eaf2fabe2cad7
vyaspot <23366840@studentmail.ucol.ac.nz>
2026-06-23 14:58:59 +1200

    fix passwword typo and add created_at to User interface

--- NutriTrack/assets/db/types.tsx
+++ NutriTrack/assets/db/types.tsx
@@ -1,18 +1,17 @@
-export interface User
-{
+export interface User {
   id: number;
   email: string;
-  passwword: string;
+  password: string;
+  created_at: string;
 }
 
-export interface Profile
-{
-	id: number,
-	user_id: number,
-	name: string,
-	birthdate: string,
-	gender: string,
-	ethnicity: string,
-	dietary_requirements: string,
-	medical_conditions: string,
+export interface Profile {
+  id: number;
+  user_id: number;
+  name: string;
+  birthdate: string;
+  gender: string;
+  ethnicity: string;
+  dietary_requirements: string;
+  medical_conditions: string;
 }
